fdade9c821463ed80428038f2be790b89913bf80
Mat Mannion <mathew.mannion@linklaters.com>
2023-09-22 17:36:26 +0100

    Add reproduction case

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,41 @@
+'use client';
+import { useCallback, useState } from "react";
+
+const availableKeys = {
+  a: 1,
+  b: 2,
+  c: 3
+}
+
+const state = {}
+
+function useForceUpdate() {
+  const [, setValue] = useState(0)
+
+  return useCallback(() => setValue(value => value + 1), [])
+}
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  const forceUpdate = useForceUpdate()
+
+  const onChange = useCallback(key => {
+    let val = availableKeys[key]?.defaultValue || ''
+    let newKey
+    if (key === 'name-with-position') {
+      newKey = 'name'
+      val = { position: '', suffix: '_name' }
+    } else if (key === 'bulkUploadV2') {
+      val = {
+        ENTER_COLUMN_TITLE_HERE: {},
+      }
+    }
+    val = typeof val === 'object' ? JSON.stringify(val, undefined, 2) : val
+    state[newKey || key] = { val, autoFocus: true }
+    forceUpdate()
+  }, [forceUpdate])
+
+  return <><p>State is {JSON.stringify(state)}</p>
+    <button type="button" onClick={() => onChange('buh')}>change me</button>
+  </>
 }
